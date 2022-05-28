e7267e7b84b8e76d53c3860705124dbe0e9a6f13
patokernel <patokernel@pm.me>
2022-05-27 22:25:38 -0300

    fix about on mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h1 className='about__title'>Lavadero El Mangal</h1>
       <p className='about__text'>
         Te esperamos de Lunes a Sábado en nuestro local. <br /><br />
-        Siempre buscamos la excelencia en el lavado del vehículo, por eso trabajamos con productos recomendados, que no dañan el color de la pintura, no son corrosivos y no afectan el medioambiente.<br /><br />
+        Buscamos siempre la excelencia en el lavado del vehículo, por eso trabajamos con productos recomendados, que no dañan el color de la pintura, no son corrosivos y no afectan el medioambiente.<br /><br />
         Contamos con una sala de espera equipada para el confort del cliente, con aire acondicionado, televisión, WiFi, café, gaseosas, cervezas y más... <br /><br />
         Próximamente estaremos anexando servicios de Detailing Car, e inclusive el Tratamiento con Nanocerámica.
       </p>
